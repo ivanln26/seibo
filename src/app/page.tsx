deac402a65113934e33ba372fc2be755c78f0e85
Ivan Nunez <ivanluis.cba@gmail.com>
2023-07-02 01:57:28 -0300

    Add dynamic color class to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,21 +40,64 @@ export default async function Home() {
           <input name="content" type="text" />
           <input name="topics" type="text" />
           <button type="submit">submit</button>
-          <br />
-          <Button>Filled</Button>
-          <Button icon="add">Filled</Button>
-          <br />
-          <Button color="primary" kind="tonal">Tonal</Button>
-          <Button color="primary" kind="tonal" icon="add">Tonal</Button>
-          <br />
-          <Button color="primary" kind="outlined">Outlined</Button>
-          <Button color="primary" kind="outlined" icon="add">Outlined</Button>
-          <br />
-          <Button color="primary" kind="text">Text</Button>
-          <Button color="primary" kind="text" icon="add">Text</Button>
-          <br />
-          <Button color="primary" kind="elevated">Elevated</Button>
-          <Button color="primary" kind="elevated" icon="add">Elevated</Button>
+          <div className="flex gap-x-2 p-2">
+            <Button>Filled</Button>
+            <Button icon="add">Filled</Button>
+            <Button color="secondary">Filled</Button>
+            <Button color="secondary" icon="add">Filled</Button>
+            <Button color="tertiary">Filled</Button>
+            <Button color="tertiary" icon="add">Filled</Button>
+            <Button color="error">Filled</Button>
+            <Button color="error" icon="add">Filled</Button>
+          </div>
+          <div className="flex gap-x-2 p-2">
+            <Button color="primary" kind="tonal">Tonal</Button>
+            <Button color="primary" kind="tonal" icon="add">Tonal</Button>
+            <Button color="secondary" kind="tonal">Tonal</Button>
+            <Button color="secondary" kind="tonal" icon="add">Tonal</Button>
+            <Button color="tertiary" kind="tonal">Tonal</Button>
+            <Button color="tertiary" kind="tonal" icon="add">Tonal</Button>
+            <Button color="error" kind="tonal">Tonal</Button>
+            <Button color="error" kind="tonal" icon="add">Tonal</Button>
+          </div>
+          <div className="flex gap-x-2 p-2">
+            <Button color="primary" kind="outlined">Outlined</Button>
+            <Button color="primary" kind="outlined" icon="add">Outlined</Button>
+            <Button color="secondary" kind="outlined">Outlined</Button>
+            <Button color="secondary" kind="outlined" icon="add">
+              Outlined
+            </Button>
+            <Button color="tertiary" kind="outlined">Outlined</Button>
+            <Button color="tertiary" kind="outlined" icon="add">
+              Outlined
+            </Button>
+            <Button color="error" kind="outlined">Outlined</Button>
+            <Button color="error" kind="outlined" icon="add">Outlined</Button>
+          </div>
+          <div className="flex gap-x-2 p-2">
+            <Button color="primary" kind="text">Text</Button>
+            <Button color="primary" kind="text" icon="add">Text</Button>
+            <Button color="secondary" kind="text">Text</Button>
+            <Button color="secondary" kind="text" icon="add">Text</Button>
+            <Button color="tertiary" kind="text">Text</Button>
+            <Button color="tertiary" kind="text" icon="add">Text</Button>
+            <Button color="error" kind="text">Text</Button>
+            <Button color="error" kind="text" icon="add">Text</Button>
+          </div>
+          <div className="flex gap-x-2 p-2">
+            <Button color="primary" kind="elevated">Elevated</Button>
+            <Button color="primary" kind="elevated" icon="add">Elevated</Button>
+            <Button color="secondary" kind="elevated">Elevated</Button>
+            <Button color="secondary" kind="elevated" icon="add">
+              Elevated
+            </Button>
+            <Button color="tertiary" kind="elevated">Elevated</Button>
+            <Button color="tertiary" kind="elevated" icon="add">
+              Elevated
+            </Button>
+            <Button color="error" kind="elevated">Elevated</Button>
+            <Button color="error" kind="elevated" icon="add">Elevated</Button>
+          </div>
         </form>
       </section>
       <section>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -18,42 +18,71 @@ type ButtonProps = {
   icon?: Icon;
 };
 
-function cls(
-  color: Color = "primary",
-  kind: Kind = "filled",
-  icon?: Icon,
-): string {
-  let className = `flex items-center h-10 ${
-    !icon ? "px-6" : "pl-4 pr-6 gap-x-2"
-  } rounded-full `;
-  switch (kind) {
-    case "tonal":
-      className += "text-[#301400] fill-[#301400] bg-[#FFDCC6]";
-      break;
-    case "outlined":
-      className += "text-[#954A00] fill-[#954A00] border border-[#84746A]";
-      break;
-    case "text":
-      className += "text-[#954A00] fill-[#954A00]";
-      break;
-    case "elevated":
-      className += "text-[#954A00] fill-[#954A00] shadow-lg";
-      break;
-    case "filled":
-    default:
-      className += "text-white fill-white bg-[#954A00]";
-  }
-  return className;
-}
+const config: Record<Kind, Record<Color, string>> = {
+  filled: {
+    primary:
+      "text-white fill-white bg-primary-600 dark:text-primary-800 dark:fill-primary-800 dark:bg-primary-200",
+    secondary:
+      "text-white fill-white bg-secondary-600 dark:text-secondary-800 dark:fill-secondary-800 dark:bg-secondary-200",
+    tertiary:
+      "text-white fill-white bg-tertiary-600 dark:text-tertiary-800 dark:fill-tertiary-800 dark:bg-tertiary-200",
+    error:
+      "text-white fill-white bg-error-600 dark:text-error-800 dark:fill-error-800 dark:bg-error-200",
+  },
+  tonal: {
+    primary:
+      "text-primary-900 fill-primary-900 bg-primary-100 dark:text-primary-100 dark:fill-primary-100 dark:bg-primary-700",
+    secondary:
+      "text-secondary-900 fill-secondary-900 bg-secondary-100 dark:text-secondary-100 dark:fill-secondary-100 dark:bg-secondary-700",
+    tertiary:
+      "text-tertiary-900 fill-tertiary-900 bg-tertiary-100 dark:text-tertiary-100 dark:fill-tertiary-100 dark:bg-tertiary-700",
+    error:
+      "text-error-900 fill-error-900 bg-error-100 dark:text-error-100 dark:fill-error-100 dark:bg-error-700",
+  },
+  outlined: {
+    primary:
+      "text-primary-600 fill-primary-600 outline outline-1 outline-outline dark:text-primary-200 dark:fill-primary-200",
+    secondary:
+      "text-secondary-600 fill-secondary-600 outline outline-1 outline-outline dark:text-secondary-200 dark:fill-secondary-200",
+    tertiary:
+      "text-tertiary-600 fill-tertiary-600 outline outline-1 outline-outline dark:text-tertiary-200 dark:fill-tertiary-200",
+    error:
+      "text-error-600 fill-error-600 outline outline-1 outline-outline dark:text-error-200 dark:fill-error-200",
+  },
+  text: {
+    primary:
+      "text-primary-600 fill-primary-600 dark:text-primary-200 dark:fill-primary-200",
+    secondary:
+      "text-secondary-600 fill-secondary-600 dark:text-secondary-200 dark:fill-secondary-200",
+    tertiary:
+      "text-tertiary-600 fill-tertiary-600 dark:text-tertiary-200 dark:fill-tertiary-200",
+    error:
+      "text-error-600 fill-error-600 dark:text-error-200 dark:fill-error-200",
+  },
+  elevated: {
+    primary:
+      "text-primary-600 fill-primary-600 shadow-lg dark:text-primary-200 dark:fill-primary-200 dark:shadow-none",
+    secondary:
+      "text-secondary-600 fill-secondary-600 shadow-lg dark:text-secondary-200 dark:fill-secondary-200 dark:shadow-none",
+    tertiary:
+      "text-tertiary-600 fill-tertiary-600 shadow-lg dark:text-tertiary-200 dark:fill-tertiary-200 dark:shadow-none",
+    error:
+      "text-error-600 fill-error-600 shadow-lg dark:text-error-200 dark:fill-error-200 dark:shadow-none",
+  },
+} as const;
 
-export default function Button(props: ButtonProps) {
+export default function Button(
+  { children, kind = "filled", color = "primary", icon, type }: ButtonProps,
+) {
   return (
     <button
-      className={cls(props.color, props.kind, props.icon)}
-      type={props.type}
+      className={`flex items-center h-10 ${
+        !icon ? "px-6" : "pl-4 pr-6 gap-x-2"
+      } rounded-full ${config[kind][color]}`}
+      type={type}
     >
-      {props.icon && <Add height={18} width={18} />}
-      {props.children}
+      {icon && <Add height={18} width={18} />}
+      {children}
     </button>
   );
 }
