1a2925cfa4807ae39bfc519e617f8b8813d406b7
Ivan Nunez <ivanluis.cba@gmail.com>
2023-07-24 18:16:38 -0300

    Add topbar component with button to open drawer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex bg-neutral-99 dark:text-white dark:bg-neutral-4">
+      <body className="flex flex-wrap bg-neutral-99 dark:text-white dark:bg-neutral-4">
         <Navigation />
         {children}
       </body>
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 import NavigationDrawer from "@/components/nav-drawer";
 import NavigationRail from "@/components/rail";
+import TopBar from "@/components/top-bar";
 
 export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,6 +18,7 @@ export default function Navigation() {
 
   return (
     <>
+      <TopBar open={open} />
       <NavigationDrawer isOpen={isOpen} close={close} />
       <NavigationRail open={open} />
     </>
