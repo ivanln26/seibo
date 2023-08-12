e430481908f663b3e891a0fff35a2dd59da733b2
Mateo P. Cetti <mateocetti2000@gmail.com>
2023-08-12 18:48:11 -0300

    Add attendance list creation logic

--- src/app/lecture/[id]/page.tsx
+++ src/app/lecture/[id]/page.tsx
@@ -1,44 +1,27 @@
 import { headers } from "next/headers";
-import { redirect } from 'next/navigation'
 
 import { format } from 'date-fns'
 import { es } from 'date-fns/locale'
 
-import { eq } from "drizzle-orm";
+import { updateAssistances } from "./updateAssistances";
+import { getLectureCourses, getAttendances, getStudents } from "./queries";
 
-import { db } from "@/db/db";
-import {
-  attendance,
-  student,
-} from "@/db/schema";
-
+import AssistanceRow from "./AssistanceRow";
 import LecturePicker from "./lecturePicker"
 import Button from "@/components/button";
 import Modal from "@/components/modal";
-import Switch from "@/components/switch"
 
 export default async function Page() {
   const pathName = headers().get("x-invoke-path") || "";
-  const lectureID = pathName.split("").pop()
+  const lectureID = Number(pathName.split("/").pop())
   const today = new Date();
   const monthName = format(today, 'MMMM', { locale: es });
 
-  const attendances = await db.select()
-    .from(attendance)
-    .innerJoin(student, eq(attendance.studentId, student.id))
-    .where(eq(attendance.lectureId, Number(lectureID)));
+  const lectureCourse = await getLectureCourses(lectureID)
+  const attendances = await getAttendances(lectureID)
+  const students = await getStudents(lectureCourse[0].instance.courseId)
 
-  async function updateAssistances(formData: FormData) {
-    'use server'
-    const data = formData.entries();
-    data.next();
-    for (let pair of data) {
-      await db.update(attendance)
-        .set({ isPresent: pair[1] === "on" })
-        .where(eq(attendance.id, Number(pair[0])))
-    }
-    // redirect(pathName)
-  }
+  const listIsCreated = students.length === attendances.length;
 
   return (
     <div className="flex flex-col gap-5 h-screen mx-2">
@@ -51,14 +34,22 @@ export default async function Page() {
       </section>
       <section className="flex px-4 flex-col gap-2 mt-2 w-screen md:w-[1080px] ">
         <form action={updateAssistances}>
-          {attendances.map((a) => {
-            return (
-              <div className="flex  flex-row gap-2 pb-1 border-b w-full justify-between">
-                <p className="text-xl">{a.student.lastName}, {a.student.firstName}</p>
-                <Switch id={String(a.attendance.id)} name={String(a.attendance.id)} checked={a.attendance.isPresent} />
-              </div>
-            )
-          })}
+          <input type="hidden" name="lectureID" value={lectureID} />
+          <input type="hidden" name="listIsCreated" value={Number(listIsCreated)} />
+          {listIsCreated ? attendances.map((a) => {
+            return <AssistanceRow firstName={a.student.firstName}
+              lastName={a.student.lastName} id={String(a.attendance.id)}
+              isPresent={a.attendance.isPresent} />
+          })
+            :
+            students.map((s) => {
+              return (
+                <AssistanceRow firstName={s.student.firstName}
+                  lastName={s.student.lastName} id={String(s.student.id)}
+                  isPresent={false} />
+              )
+            })
+          }
           <div className="fixed bottom-2 right-5 flex flex-row gap-5">
             <Button color="error" kind="tonal">Deshacer</Button>
             <Modal buttonText="Guardar">
