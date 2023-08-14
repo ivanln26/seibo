de577d34c5f4e0d744ad573091bed9ed091160cc
Mateo Pablo Cetti <mateocetti2000@gmail.com>
2023-08-14 17:45:19 -0300

    Fix create modal in courses abm

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -58,28 +58,30 @@ export default async function Home() {
         <Table cols={columnNames} rows={createTableRows()}></Table>
       </section>
       <div className="fixed bottom-5 right-10">
-        <Modal
-          buttonText="Crear curso"
-          confirmButton={{ text: "Crear", type: "submit" }}
-        >
-          <div>
-            <h1 className="text-2xl">Nuevo curso</h1>
-            <form action={create} className="flex flex-col gap-1 mx-5">
-              <TextField
-                id="content"
-                name="content"
-                label="Contenido"
-                required
-              />
-              <TextField
-                id="topics"
-                name="topics"
-                label="Temas"
-                required
-              />
-            </form>
-          </div>
-        </Modal>
+        <form action={create}>
+          <Modal
+            buttonText="Crear curso"
+            confirmButton={{ text: "Crear", type: "submit" }}
+          >
+            <div>
+              <h1 className="text-2xl">Nuevo curso</h1>
+              <div className="flex flex-col gap-1 mx-5">
+                <TextField
+                  id="content"
+                  name="content"
+                  label="Contenido"
+                  required
+                />
+                <TextField
+                  id="topics"
+                  name="topics"
+                  label="Temas"
+                  required
+                />
+              </div>
+            </div>
+          </Modal>
+        </form>
       </div>
     </>
   );
