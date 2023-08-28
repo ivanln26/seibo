09cc97376bc44cb4a84aed3f6b3c1ca7a4d500fd
Mateo Pablo Cetti <mateocetti2000@gmail.com>
2023-08-28 20:42:55 -0300

    Fix wrong initial attendance render

--- src/app/(main)/lecture/page.tsx
+++ src/app/(main)/lecture/page.tsx
@@ -1,19 +1,39 @@
-import { redirect } from "next/navigation";
+ import { redirect } from "next/navigation";
 
-import { eq } from "drizzle-orm";
+import { eq, and, between } from "drizzle-orm";
 
 import { db } from "@/db/db";
 import { course, grade, instance, lecture, schedule } from "@/db/schema";
 
+function getMonday(d: Date) {
+  d = new Date(d);
+  var day = d.getDay(),
+      diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
+  const newDate = new Date(d.setDate(diff));
+  return new Date(newDate.setUTCHours(0,0,0,0));
+}
+
+function getFriday(d: Date) {
+  d = new Date(d);
+  var day = d.getDay(),
+      diff = d.getDate() - day + (day <= 5 ? (5 - day) : (12 - day)); // adjust when day is after Friday
+  const newDate = new Date(d.setDate(diff));
+  return new Date(newDate.setUTCHours(23,59,59)); 
+}
+
 export default async function Page() {
   const today = new Date();
 
+  // query recurrente. Separar en un archivo y llamar
   const lectures = await db.select().from(lecture)
     .innerJoin(schedule, eq(lecture.scheduleId, schedule.id))
     .innerJoin(instance, eq(schedule.instanceId, instance.id))
     .innerJoin(course, eq(instance.courseId, course.id))
     .innerJoin(grade, eq(instance.gradeId, grade.id))
-    .where(eq(instance.professorId, 1));
+    .where(and(
+      eq(instance.professorId, 1),
+      between(lecture.date, getMonday(today), getFriday(today))
+    ));
 
   let closerLecture = lectures[0].lecture.id;
   let closerDateDiff = lectures[0].lecture.date.getUTCDate() -
