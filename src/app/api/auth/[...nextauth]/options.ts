03a86757bcd6edaf6a3da496798d69d95e48bf0a
Ivan Nuñez <ivanluis.cba@gmail.com>
2023-09-04 18:05:51 -0300

    Save user's name and email in the db

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,20 @@
+import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+import { db } from "@/db/db";
+import { user } from "@/db/schema";
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    async jwt({ token }) {
+      console.log("Token", token);
+      await db.insert(user).values({
+        email: token.email || "",
+        name: token.name || "",
+      }).onDuplicateKeyUpdate({ set: { name: token.name || "" } });
+      return token;
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
