dc215eea56cee98e043db16c2305cb2b2d46245c
Ivan Nuñez <ivanluis.cba@gmail.com>
2023-09-10 18:24:54 -0300

    Include user roles in auth session token

--- src/app/(main)/user/page.tsx
+++ src/app/(main)/user/page.tsx
@@ -10,7 +10,8 @@ export default function Page() {
   return (
     <>
       <h1>Usuario</h1>
-      <h2>{session?.user?.email}</h2>
+      <h2>Email: {session?.user?.email}</h2>
+      <h2>Roles: {session?.user?.roles.map((r) => <>{r},</>)}</h2>
       <Button onClick={() => signOut()}>
         Log Out
       </Button>
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,18 +1,57 @@
-import type { NextAuthOptions } from "next-auth";
+import type { DefaultSession, NextAuthOptions } from "next-auth";
+import type { DefaultJWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
+import { z } from "zod";
 
 import { db } from "@/db/db";
 import { user } from "@/db/schema";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      roles: string[];
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    roles: string[];
+  }
+}
+
+const userSchema = z.object({
+  email: z.string().email(),
+  name: z.string(),
+});
+
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    async jwt({ token }) {
-      console.log("Token", token);
-      await db.insert(user).values({
-        email: token.email || "",
-        name: token.name || "",
-      }).onDuplicateKeyUpdate({ set: { name: token.name || "" } });
+    async jwt({ token, trigger }) {
+      if (trigger === "signIn") {
+        const { email, name } = userSchema.parse({
+          email: token.email,
+          name: token.name,
+        });
+
+        await db.insert(user).values({ email, name }).onDuplicateKeyUpdate({
+          set: { name },
+        });
+
+        const u = await db.query.user.findFirst({
+          where: (user, { eq }) => eq(user.email, email),
+          with: { profiles: true },
+        });
+
+        if (!u) throw new Error("user not in db");
+
+        token.roles = u.profiles.map((p) => p.role);
+      }
       return token;
+    },
+    async session({ session, token }) {
+      session.user.roles = token.roles;
+      return session;
     },
   },
   providers: [
