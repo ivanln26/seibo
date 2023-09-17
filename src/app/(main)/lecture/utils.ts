917869724c86f9e4aa46ffa65c68beb2a0bd6d3a
Mateo P. Cetti <mateocetti2000@gmail.com>
2023-09-17 15:47:07 -0300

    Move recurrent functions to utils file

--- src/app/(main)/lecture/utils.ts
+++ src/app/(main)/lecture/utils.ts
@@ -1,9 +1,11 @@
-import { eq } from "drizzle-orm";
+import { and, between, eq } from "drizzle-orm";
 
 import { db } from "@/db/db";
 import {
   attendance,
+  course,
   courseGrade,
+  grade,
   instance,
   lecture,
   schedule,
@@ -44,4 +46,33 @@ export async function getUser(session: Session){
       profiles: { where: (profile, { eq }) => eq(profile.isActive, true) },
     },
   });
+}
+
+export function getMonday(d: Date) {
+  d = new Date(d);
+  var day = d.getDay(),
+    diff = d.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+  const newDate = new Date(d.setDate(diff));
+  return new Date(newDate.setUTCHours(0, 0, 0, 0));
+}
+
+export function getFriday(d: Date) {
+  d = new Date(d);
+  var day = d.getDay(),
+    diff = d.getDate() - day + (day <= 5 ? (5 - day) : (12 - day)); // adjust when day is after Friday
+  const newDate = new Date(d.setDate(diff));
+  return new Date(newDate.setUTCHours(23, 59, 59));
+}
+
+export async function getweeklyLectures(professorId: number, date: Date){
+  return await db.select().from(lecture)
+    .innerJoin(schedule, eq(lecture.scheduleId, schedule.id))
+    .innerJoin(instance, eq(schedule.instanceId, instance.id))
+    .innerJoin(course, eq(instance.courseId, course.id))
+    .innerJoin(grade, eq(instance.gradeId, grade.id))
+    .where(and(
+      eq(instance.professorId, 1),
+      between(lecture.date, getMonday(date), getFriday(date)),
+    ))
+    .orderBy(schedule.weekday, schedule.startTime);
 }
