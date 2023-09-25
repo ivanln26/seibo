46b48ccadd366ffdc04464bb1aaf5c901dd21010
Mateo Pablo Cetti <mateocetti2000@gmail.com>
2023-09-25 17:54:11 -0300

    (#45) Remove unused imports

--- src/app/(main)/abm/user/page.tsx
+++ src/app/(main)/abm/user/page.tsx
@@ -1,21 +1,19 @@
 import { db } from "@/db/db";
-import { course, grade, instance, schedule, schoolUser, user as User } from "@/db/schema";
+import { schoolUser, user } from "@/db/schema";
 import { and, eq } from "drizzle-orm";
 import { getServerSession } from "next-auth";
 import { getUser } from "../../lecture/utils";
-import Modal from "@/components/modal";
 import Link from "next/link";
-import { z } from "zod";
-import { revalidatePath } from "next/cache";
 
 export default async function Page() {
     const session = await getServerSession();
     if (!session) return (<>Error al obtener la sesión.</>)
-    const user = await getUser(session);
-    if (!user) return <>Error al obtener el usuario.</>
+    const currentUser = await getUser(session);
+    if (!currentUser) return <>Error al obtener el usuario.</>
+    
     const users = await db.select()
-        .from(User)
-        .innerJoin(schoolUser, eq(User.id, schoolUser.userId))
+        .from(user)
+        .innerJoin(schoolUser, eq(user.id, schoolUser.userId))
         .where(and(
             eq(schoolUser.schoolId, 1) // getear mediante el slug
         ))
@@ -33,10 +31,10 @@ export default async function Page() {
                             <td className="border border-black">Estado</td>
                         </tr>
                         {users.map((u) => <tr>
-                            <td className="border border-black"><Link href={`/schedule/${u.user.id}`}>{u.user.name}</Link></td>
-                            <td className="border border-black"><Link href={`/schedule/${u.user.id}`}>{u.user.email}</Link></td>
-                            <td className="border border-black"><Link href={`/schedule/${u.user.id}`}>{u.school_user.role}</Link></td>
-                            <td className="border border-black"><Link href={`/schedule/${u.user.id}`}>{u.school_user.isActive ? "alta" : "baja"}</Link></td>
+                            <td className="border border-black"><Link href={`/abm/user/${u.user.id}`}>{u.user.name}</Link></td>
+                            <td className="border border-black"><Link href={`/abm/user/${u.user.id}`}>{u.user.email}</Link></td>
+                            <td className="border border-black"><Link href={`/abm/user/${u.user.id}`}>{u.school_user.role}</Link></td>
+                            <td className="border border-black"><Link href={`/abm/user/${u.user.id}`}>{u.school_user.isActive ? "alta" : "baja"}</Link></td>
                         </tr>)}
                     </tbody>
                 </table>
