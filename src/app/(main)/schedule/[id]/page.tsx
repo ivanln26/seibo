092e5dcdd56fe6a9bb86fbd73d23f3348e69b643
Mateo P. Cetti <mateocetti2000@gmail.com>
2023-09-24 22:42:15 -0300

    Add delete schedule functionality

--- src/app/(main)/schedule/[id]/page.tsx
+++ src/app/(main)/schedule/[id]/page.tsx
@@ -5,6 +5,7 @@ import TextField from "@/components/text-field";
 import Button from "@/components/button";
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 type Props = {
   params: {
@@ -13,11 +14,11 @@ type Props = {
 };
 
 const weekdays = [
-  {name: "Lunes", value: "monday"},
-  {name: "Martes", value: "tuesday"},
-  {name: "Miercoles", value: "wednesday"},
-  {name: "Jueves", value: "thursday"},
-  {name: "Viernes", value: "friday"},
+  { name: "Lunes", value: "monday" },
+  { name: "Martes", value: "tuesday" },
+  { name: "Miercoles", value: "wednesday" },
+  { name: "Jueves", value: "thursday" },
+  { name: "Viernes", value: "friday" },
 ]
 
 export default async function Page({ params }: Props) {
@@ -29,38 +30,44 @@ export default async function Page({ params }: Props) {
     .innerJoin(user, eq(instance.professorId, user.id))
     .where(eq(course.schoolId, 1))
 
-    async function save(data: FormData){
-      "use server"
-      const t = z.object({
-        instanceID: z.number(),
-        weekday: z.enum(["monday", "tuesday","wednesday", "thursday", "friday"]),
-        startTime: z.string(),
-        endTime: z.string(),
-      });
-      const newSchedule = t.safeParse({
-        instanceID: Number(data.get("instanceID")),
-        weekday: data.get("weekday"),
-        startTime: data.get("startTime"),
-        endTime: data.get("endTime"),
-      });
-      if (!newSchedule.success) {
-        console.log("error", newSchedule.error)
-        return;
-      }
-  
-      await db.update(schedule).set({
-        instanceId: newSchedule.data.instanceID,
-        weekday: newSchedule.data.weekday,
-        startTime:newSchedule.data.startTime,
-        endTime: newSchedule.data.endTime
-      }).where(eq(schedule.id, newSchedule.data.instanceID))
-      revalidatePath("/schedule");
+  async function save(data: FormData) {
+    "use server"
+    const t = z.object({
+      instanceID: z.number(),
+      weekday: z.enum(["monday", "tuesday", "wednesday", "thursday", "friday"]),
+      startTime: z.string(),
+      endTime: z.string(),
+    });
+    const newSchedule = t.safeParse({
+      instanceID: Number(data.get("instanceID")),
+      weekday: data.get("weekday"),
+      startTime: data.get("startTime"),
+      endTime: data.get("endTime"),
+    });
+    if (!newSchedule.success) {
+      console.log("error", newSchedule.error)
+      return;
     }
+
+    await db.update(schedule).set({
+      instanceId: newSchedule.data.instanceID,
+      weekday: newSchedule.data.weekday,
+      startTime: newSchedule.data.startTime,
+      endTime: newSchedule.data.endTime
+    }).where(eq(schedule.id, newSchedule.data.instanceID))
+    revalidatePath("/schedule");
+  }
+  async function deleteSchedule() {
+    "use server"
+    await db.delete(schedule)
+      .where(eq(schedule.id, sched[0].id))
+    redirect("/schedule")
+  }
   return (
     <>
       <h1 className="text-4xl">Modificar Horario</h1>
-      <form action={save}>
-        <div className="div rounded-xl bg-primary-100 w-full m-5 py-3 px-4 flex flex-col gap-5">
+      <div className="div rounded-xl bg-primary-100 w-full m-5 py-3 px-4">
+        <form action={save} className="flex flex-col gap-5">
           <label htmlFor="">Clase</label>
           <select name="instanceID" className="py-4 outline outline-1 rounded bg-white outline-outline">
             {instances.map((i) => <option value={Number(i.instance.id)}>{i.course.name} | {i.grade.name} | {i.user.name}</option>)}
@@ -74,8 +81,11 @@ export default async function Page({ params }: Props) {
           <TextField label="Hora de inicio" id="Start time" name="startTime" defaultValue={sched[0].startTime}></TextField>
           <TextField label="Hora de Fin" id="Start time" name="endTime" defaultValue={sched[0].endTime}></TextField>
           <Button type="submit" color="tertiary">Guardar</Button>
-        </div>
-      </form>
+        </form>
+        <form action={deleteSchedule} className="flex flex-col my-5">
+          <Button type="submit">Borrar</Button>
+        </form>
+      </div>
     </>
   )
 }
