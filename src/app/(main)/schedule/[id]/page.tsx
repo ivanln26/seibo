9c913a8c872dbe6519f1530acee851557cf7bcc4
Mateo P. Cetti <mateocetti2000@gmail.com>
2023-09-24 22:30:03 -0300

    Add update schedule functionality

--- src/app/(main)/schedule/[id]/page.tsx
+++ src/app/(main)/schedule/[id]/page.tsx
@@ -1,20 +1,81 @@
 import { db } from "@/db/db";
-import { schedule } from "@/db/schema";
+import { course, grade, instance, schedule, user } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import TextField from "@/components/text-field";
+import Button from "@/components/button";
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
 
 type Props = {
-    params: {
-      id: number;
-    };
+  params: {
+    id: number;
   };
+};
 
-export default async function Page({ params }: Props){
+const weekdays = [
+  {name: "Lunes", value: "monday"},
+  {name: "Martes", value: "tuesday"},
+  {name: "Miercoles", value: "wednesday"},
+  {name: "Jueves", value: "thursday"},
+  {name: "Viernes", value: "friday"},
+]
 
-    const sched = await db.select().from(schedule).where(eq(schedule.id, params.id))
-    
-    return (
-        <>
-        <h1 className="text-4xl">Modificar Horario</h1>
-        </>
-    )
+export default async function Page({ params }: Props) {
+
+  const sched = await db.select().from(schedule).where(eq(schedule.id, params.id))
+  const instances = await db.select().from(instance)
+    .innerJoin(course, eq(instance.courseId, course.id))
+    .innerJoin(grade, eq(instance.gradeId, grade.id))
+    .innerJoin(user, eq(instance.professorId, user.id))
+    .where(eq(course.schoolId, 1))
+
+    async function save(data: FormData){
+      "use server"
+      const t = z.object({
+        instanceID: z.number(),
+        weekday: z.enum(["monday", "tuesday","wednesday", "thursday", "friday"]),
+        startTime: z.string(),
+        endTime: z.string(),
+      });
+      const newSchedule = t.safeParse({
+        instanceID: Number(data.get("instanceID")),
+        weekday: data.get("weekday"),
+        startTime: data.get("startTime"),
+        endTime: data.get("endTime"),
+      });
+      if (!newSchedule.success) {
+        console.log("error", newSchedule.error)
+        return;
+      }
+  
+      await db.update(schedule).set({
+        instanceId: newSchedule.data.instanceID,
+        weekday: newSchedule.data.weekday,
+        startTime:newSchedule.data.startTime,
+        endTime: newSchedule.data.endTime
+      }).where(eq(schedule.id, newSchedule.data.instanceID))
+      revalidatePath("/schedule");
+    }
+  return (
+    <>
+      <h1 className="text-4xl">Modificar Horario</h1>
+      <form action={save}>
+        <div className="div rounded-xl bg-primary-100 w-full m-5 py-3 px-4 flex flex-col gap-5">
+          <label htmlFor="">Clase</label>
+          <select name="instanceID" className="py-4 outline outline-1 rounded bg-white outline-outline">
+            {instances.map((i) => <option value={Number(i.instance.id)}>{i.course.name} | {i.grade.name} | {i.user.name}</option>)}
+          </select>
+          <label htmlFor="">Dia de la semana</label>
+          <select name="weekday" className="py-4 outline outline-1 rounded bg-white outline-outline" defaultValue={sched[0].weekday}>
+            {
+              weekdays.map((w) => <option value={w.value} >{w.name}</option>)
+            }
+          </select>
+          <TextField label="Hora de inicio" id="Start time" name="startTime" defaultValue={sched[0].startTime}></TextField>
+          <TextField label="Hora de Fin" id="Start time" name="endTime" defaultValue={sched[0].endTime}></TextField>
+          <Button type="submit" color="tertiary">Guardar</Button>
+        </div>
+      </form>
+    </>
+  )
 }
