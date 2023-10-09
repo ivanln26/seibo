b54f27e141dd5a927698f93c8edbb219f2d828d6
Mateo Pablo Cetti <mateocetti2000@gmail.com>
2023-10-09 19:23:14 -0300

    Add add and delete student functionality

---
+++ src/app/(main)/abm/student/[id]/page.tsx
@@ -0,0 +1,150 @@
+import { redirect } from "next/navigation";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+import Button from "@/components/button";
+import TextField from "@/components/text-field";
+import { db } from "@/db/db";
+import { grade, student, studentContact, studentGrade } from "@/db/schema";
+
+type Props = {
+    params: {
+        id: string;
+    };
+};
+
+export default async function Page({ params }: Props) {
+    const currentStudent = (
+        await db.select()
+            .from(student)
+            .innerJoin(studentContact, eq(student.id, studentContact.studentId))
+            .innerJoin(studentGrade, eq(student.id, studentGrade.studentId))
+            .where(eq(student.id, Number(params.id)))
+    )[0]
+
+    if (!currentStudent) {
+        redirect("/");
+    }
+
+    console.log(currentStudent)
+
+    const grades = await db.select()
+        .from(grade) // filtrar según colegio
+
+    const update = async (data: FormData) => {
+        "use server";
+        const studentType = z.object({
+            firstName: z.string(),
+            lastName: z.string(),
+            email: z.string(),
+            schoolId: z.number(),
+            studentCode: z.string(),
+        });
+        const contactType = z.object({
+            name: z.string(),
+            email: z.string(),
+            phone: z.string(),
+            type: z.enum(["father", "mother", "tutor", "other"])
+        });
+
+        const gradeiDType = z.object({ id: z.number() })
+
+        const newStudent = studentType.safeParse({
+            firstName: data.get("studentFirstName"),
+            lastName: data.get("studentLastName"),
+            email: data.get("studentEmail"),
+            schoolId: 1, // TODO change
+            studentCode: data.get("studentCode"),
+        });
+        const newContact = contactType.safeParse({
+            name: data.get("contactName"),
+            email: data.get("contactEmail"),
+            phone: data.get("contactPhone"),
+            type: data.get("contactType"),
+        });
+
+        const newGradeId = gradeiDType.safeParse({ id: Number(data.get("studentGrade")) })
+
+        if (!newStudent.success || !newContact.success || !newGradeId.success) {
+            return;
+        }
+
+        await db.update(student).set({ ...newStudent.data })
+            .where(eq(student.id, Number(params.id)));
+
+        await db.update(studentContact).set({
+            ...newContact.data,
+            studentId: Number(params.id)
+        }).where(
+            eq(studentContact.id, currentStudent.student_contact.id)
+        )
+
+        await db.update(studentGrade).set({
+            studentId: Number(params.id),
+            gradeId: newGradeId.data.id
+        }).where(
+            eq(studentGrade.id, currentStudent.student_grade.id)
+        )
+        redirect("/abm/student");
+    };
+
+    const del = async () => {
+        "use server";
+        await db.delete(student).where(
+            eq(student.id, Number(params.id)),
+        );
+
+        await db.delete(studentContact).where(
+            eq(studentContact.id, currentStudent.student_contact.id),
+        );
+
+        await db.delete(studentGrade).where(
+            eq(studentGrade.id, currentStudent.student_grade.id),
+        );
+        redirect("/");
+    };
+
+    return (
+        <section>
+            <h1 className="text-4xl ml-5">Modificar curso</h1>
+            <div className="bg-primary-100 flex flex-col gap-5 m-5 p-4 rounded-xl">
+                <form action={update}>
+                    <div className="grid grid-cols-2 gap-4">
+                        <TextField id="" name="studentFirstName" label="Nombre" defaultValue={currentStudent.student.firstName} required />
+                        <TextField id="" name="studentLastName" label="Apellido" defaultValue={currentStudent.student.lastName} required />
+                        <TextField id="" name="studentEmail" label="E-mail" defaultValue={currentStudent.student.email} required />
+                        <TextField id="" name="studentCode" label="Legajo" defaultValue={currentStudent.student.studentCode} required />
+                        <div className="flex-none basis-1/2 grow w-full">
+                            <label htmlFor="" className="mt-4">Curso*</label>
+                            <select name="studentGrade" defaultValue={currentStudent.student_grade.gradeId} className="outline outline-1 outline-outline rounded p-4 w-full">
+                                <option value="">----</option>
+                                {grades.map((c) => <option value={Number(c.id)}>{c.name}</option>)}
+                            </select>
+                        </div>
+                    </div>
+                    <label htmlFor="" className="text-xl basis-full">Padre/Madre/Tutor</label>
+                    <input type="hidden" name="studentSchool" value={1 /* Cambiar por colegio */} />
+                    <div className="grid grid-cols-2 gap-4 border rounded-lg p-2">
+                        <TextField label="Nombre y apellido" name="contactName" id="" defaultValue={currentStudent.student_contact.name}></TextField>
+                        <TextField label="E-mail" name="contactEmail" id="" defaultValue={currentStudent.student_contact.email}></TextField>
+                        <TextField label="telefono" name="contactPhone" id="" defaultValue={currentStudent.student_contact.phone}></TextField>
+                        <div className="flex flex-col justify-center">
+                            <label htmlFor="">Tipo</label>
+                            <select name="contactType" id="" defaultValue={currentStudent.student_contact.type} className=" outline outline-1 outline-outline rounded p-4 w-full">
+                                <option value="-">---</option>
+                                <option value="father">Padre</option>
+                                <option value="mother">Madre</option>
+                                <option value="tutor">Tutor</option>
+                                <option value="other">Otro</option>
+                            </select>
+                        </div>
+                    </div>
+                    <Button color="tertiary" type="submit">Guardar</Button>
+                </form>
+                <form action={del}>
+                    <Button color="error" type="submit">Borrar</Button>
+                </form>
+            </div>
+        </section>
+    );
+}
