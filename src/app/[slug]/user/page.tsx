791cb98d0d8325f35dccee6e2be111d6cb2832b5
Ivan Nuñez <ivanluis.cba@gmail.com>
2023-11-05 20:17:26 -0300

    (#69) Stylize profile page

--- src/app/[slug]/user/page.tsx
+++ src/app/[slug]/user/page.tsx
@@ -1,37 +1,55 @@
-import { getServerSession } from "next-auth";
-
-import { db } from "@/db/db";
 import LogOutButton from "./logout-button";
+import { db } from "@/db/db";
+import type { Role } from "@/db/schema";
+import { getUserProfile } from "@/db/queries";
 
 export const revalidate = 0;
 
-export default async function Page() {
-  const session = await getServerSession();
+type Props = {
+  params: {
+    slug: string;
+  };
+};
 
-  if (!session) {
-    return <>Error al obtener la sesión.</>;
-  }
+const translation: Record<Role, string> = {
+  admin: "Administrador",
+  principal: "Director",
+  teacher: "Profesor",
+  tutor: "Preceptor",
+};
 
-  const u = await db.query.user.findFirst({
-    where: (user, { eq }) => eq(user.email, session.user.email),
-    with: {
-      profiles: { where: (profile, { eq }) => eq(profile.isActive, true) },
-    },
+export default async function Page({ params }: Props) {
+  const user = await getUserProfile({ slug: params.slug });
+  const school = await db.query.school.findFirst({
+    columns: { name: true },
+    where: (school, { eq }) => eq(school.slug, params.slug),
   });
 
-  if (!u) {
-    return <>Error al obtener el usuario de la base de datos.</>;
+  if (!school) {
+    return <>Error al obtener la escuela de la base de datos.</>;
   }
 
   return (
-    <>
-      <h1>Usuario</h1>
-      <h2>Email: {session?.user?.email}</h2>
-      <h2>Roles:</h2>
-      <ul>
-        {u.profiles.map((p) => <li>{p.role}</li>)}
-      </ul>
-      <LogOutButton />
-    </>
+    <div className="flex flex-col md:flex-row gap-x-6 gap-y-4">
+      <section className="flex flex-col gap-y-3 p-4 rounded outline outline-1 md:outline-0 outline-outline">
+        <h1 className="text-4xl font-bold">{user.name}</h1>
+        <h2 className="text-lg">{user.email}</h2>
+        <LogOutButton />
+      </section>
+      <section className="flex flex-col gap-y-2 p-4 rounded outline outline-1 md:outline-0 outline-outline">
+        <h2 className="text-4xl font-bold">{school.name}</h2>
+        <h1 className="text-2xl">Roles</h1>
+        <ul className="list-disc list-inside text-lg">
+          {user.profiles.map((p) => (
+            <li className="ml-4">{translation[p.role]}</li>
+          ))}
+        </ul>
+        <h1 className="text-2xl">Materias</h1>
+        <ul className="list-disc list-inside text-lg">
+          <li className="ml-4">Lengua</li>
+          <li className="ml-4">Matemática</li>
+        </ul>
+      </section>
+    </div>
   );
 }
