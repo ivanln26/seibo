a85670160d319783ba96f14cd83cdc0e0d518cb3
Ivan Nuñez <ivanluis.cba@gmail.com>
2023-11-04 21:08:25 -0300

    (#60) Create admin layout and navigation components

---
+++ src/components/admin/navigation.tsx
@@ -0,0 +1,32 @@
+import AdminNavigationButton from "./navigation-button";
+import type { AdminNavigationButtonProps } from "./navigation-button";
+
+type Props = {
+  slug: string;
+};
+
+const adminNavButtons: AdminNavigationButtonProps[] = [
+  { name: "Aula", href: "classroom" },
+  { name: "Horario", href: "schedule" },
+  { name: "Materia", href: "course" },
+  { name: "Estudiante", href: "student" },
+  { name: "Usuario", href: "user" },
+];
+
+export default function AdminNavigation({ slug }: Props) {
+  return (
+    <nav className="px-4">
+      <h1 className="text-xl font-bold">Administrador</h1>
+      <ul className="mt-2">
+        {adminNavButtons.map(({ name, href }) => (
+          <li>
+            <AdminNavigationButton
+              name={name}
+              href={`/${slug}/admin/${href}`}
+            />
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
