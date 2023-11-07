2759917639d74cd384de13e8a7d08e0de46b75c8
Mateo Pablo Cetti <mateocetti2000@gmail.com>
2023-11-06 23:52:54 -0300

    Add send mail functionality

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,25 +1,61 @@
-import { db } from "@/db/db";
+import Link from "next/link";
+import { getUserSchools } from "@/db/queries";
+import Button from "@/components/button";
+import { redirect } from "next/dist/server/api-utils";
+
+var nodemailer = require("nodemailer");
+
+async function sendMail(subject: string, toEmail: string, otpText: string) {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PW,
+    },
+  });
+
+  var mailOptions = {
+    from: process.env.NODEMAILER_EMAIL,
+    to: toEmail,
+    subject: subject,
+    text: otpText,
+  };
+
+  return await transporter.sendMail(mailOptions);
+}
 
 export const revalidate = 0;
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
+export default async function Page() {
+  const schools = await getUserSchools();
 
-export default async function Home({ params }: Props) {
-  const school = await db.query.school.findFirst({
-    where: (school, { eq }) => eq(school.slug, params.slug),
-  });
-
-  if (!school) {
-    return <>No se encontró la escuela.</>;
+  async function sape(data: FormData){
+    "use server"
+    const subject = String(data.get("subject"))
+    const toEmail = String(data.get("email"))
+    const optText = String(data.get("optText"))
+    if (!subject) return;
+    const res = await sendMail(subject, toEmail, optText)
+    if(res.accepted.length !== 1){
+      console.log("error al enviar el mail")
+      return;
+    }
+    console.log(res)
   }
 
   return (
-    <section>
-      <h1 className="text-4xl">{school.name}</h1>
-    </section>
+    <main className="flex justify-center items-center w-full h-[100svh]">
+      <section className="flex flex-col justify-center items-center gap-y-8 px-4 py-8 bg-primary-100 dark:bg-primary-700 rounded-xl">
+        <h1 className="text-4xl text-primary-900 dark:text-primary-100">
+          Enviar mail
+        </h1>
+        <form action={sape}>
+          <input name="email" type="email" />
+          <input name="subject" type="text" />
+          <input name="optText" type="text" />
+          <Button type="submit">enviar</Button>
+        </form>
+      </section>
+    </main>
   );
 }
