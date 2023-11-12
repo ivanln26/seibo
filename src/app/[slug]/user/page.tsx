ac5a72c685157566a0643053902dc1674d54b258
Ivan Nuñez <ivanluis.cba@gmail.com>
2023-11-12 18:57:47 -0300

    (#84) Refactor courses query in user page

--- src/app/[slug]/user/page.tsx
+++ src/app/[slug]/user/page.tsx
@@ -1,5 +1,8 @@
+import { eq } from "drizzle-orm";
+
 import LogOutButton from "./logout-button";
 import { db } from "@/db/db";
+import { course, courseProfessor } from "@/db/schema";
 import type { Role } from "@/db/schema";
 import { getUserProfile } from "@/db/queries";
 
@@ -29,11 +32,14 @@ export default async function Page({ params }: Props) {
     return <>Error al obtener la escuela de la base de datos.</>;
   }
 
-  const courses = await db.query.courseProfessor.findMany({
-    where: (courseProfessor, { eq }) =>
-      eq(courseProfessor.professorId, user.id),
-    with: { course: true },
-  });
+  const courses = await db
+    .select({
+      id: course.id,
+      name: course.name,
+    })
+    .from(course)
+    .innerJoin(courseProfessor, eq(course.id, courseProfessor.courseId))
+    .where(eq(courseProfessor.professorId, user.id));
 
   return (
     <div className="flex flex-col md:flex-row gap-x-6 gap-y-4">
@@ -52,7 +58,7 @@ export default async function Page({ params }: Props) {
         </ul>
         <h1 className="text-2xl">Materias</h1>
         <ul className="list-disc list-inside text-lg">
-          {courses.map((c) => <li className="ml-4">{c.course.name}</li>)}
+          {courses.map(({ name }) => <li className="ml-4">{name}</li>)}
         </ul>
       </section>
     </div>
