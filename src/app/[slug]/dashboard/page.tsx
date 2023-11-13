10be14a88a1c7503de2ca5c8512b3fecd98d5cb2
Mateo P. Cetti <mateocetti2000@gmail.com>
2023-11-12 21:02:17 -0300

    Add attendances chart

--- src/app/[slug]/dashboard/page.tsx
+++ src/app/[slug]/dashboard/page.tsx
@@ -1,8 +1,9 @@
-import { eq, sql } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 
 import Canvas from "./canvas";
 import { db } from "@/db/db";
-import { grade, school, studentGrade } from "@/db/schema";
+import { attendance, grade, instance, lecture, schedule, school, student, studentGrade } from "@/db/schema";
+import Attendances from "./attendances";
 
 export const revalidate = 0;
 
@@ -12,7 +13,7 @@ type Props = {
   };
 };
 
-export default async function Page({ params } : Props) {
+export default async function Page({ params }: Props) {
   const query = await db
     .select({
       gradeId: grade.id,
@@ -25,9 +26,60 @@ export default async function Page({ params } : Props) {
     .where(eq(school.slug, params.slug))
     .groupBy(({ gradeId }) => gradeId);
 
+  const attendances = await db.select({
+    gradeId: grade.id,
+    grade: grade.name,
+    isPresent: attendance.isPresent,
+    count: sql<number>`count(${attendance.id})`,
+  }).from(attendance)
+    .innerJoin(lecture, eq(attendance.lectureId, lecture.id))
+    .innerJoin(schedule, eq(lecture.scheduleId, schedule.id))
+    .innerJoin(instance, eq(schedule.instanceId, instance.id))
+    .innerJoin(grade, eq(instance.gradeId, grade.id))
+    .innerJoin(school, eq(grade.schoolId, school.id))
+    .where(and(eq(attendance.isPresent, true), eq(school.slug, params.slug)))
+    .groupBy(grade.id);
+
+  const notAttendances = await db.select({
+    gradeId: grade.id,
+    grade: grade.name,
+    isPresent: attendance.isPresent,
+    count: sql<number>`count(${attendance.id})`,
+  }).from(attendance)
+    .innerJoin(lecture, eq(attendance.lectureId, lecture.id))
+    .innerJoin(schedule, eq(lecture.scheduleId, schedule.id))
+    .innerJoin(instance, eq(schedule.instanceId, instance.id))
+    .innerJoin(grade, eq(instance.gradeId, grade.id))
+    .innerJoin(school, eq(grade.schoolId, school.id))
+    .where(and(eq(attendance.isPresent, false), eq(school.slug, params.slug)))
+    .groupBy(grade.id);
+
+  const all = attendances.concat(notAttendances).sort((a, b) => a.gradeId - b.gradeId)
+  console.log(all)
   return (
     <>
-      <Canvas data={query} />
+      <div className="grid grid-cols-4 gap-5">
+        <div className="">
+          <h1 className="text-center text-xl">Cantidad de alumnos por curso</h1>
+          <Canvas data={query} />
+        </div>
+        <div className="">
+          <h1 className="text-center text-xl">Cantidad de alumnos por curso</h1>
+          <Canvas data={query} />
+        </div>
+        <div className="">
+          <h1 className="text-center text-xl">Cantidad de alumnos por curso</h1>
+          <Canvas data={query} />
+        </div>
+        <div className="">
+          <h1 className="text-center text-xl">Cantidad de alumnos por curso</h1>
+          <Canvas data={query} />
+        </div>
+        <div className="col-span-2 row-span-2 flex flex-col">
+          <h1 className="text-center text-xl">Cantidad de asistencias / inasistencias por curso</h1>
+          <Attendances data={all} />
+        </div>
+      </div>
     </>
   );
 }
