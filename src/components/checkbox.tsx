cff0e04de7449fc391189b47b3890771d399c1d6
Ivan Nuñez <ivanluis.cba@gmail.com>
2024-01-19 14:02:27 -0300

    Add animation to checkbox

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -31,8 +31,8 @@ export default function Checkbox({ id, name, checked = false }: CheckboxProps) {
         onChange={() => setCheck(!check)}
         type="checkbox"
       />
-      <span className="block h-5 w-5 cursor-pointer rounded-sm border border-2 border-neutral-variant-50 peer-checked:border-0 peer-checked:bg-primary-600 dark:border-neutral-varialnt-60 peer-checked:dark:bg-primary-200" />
-      <span className="absolute top-0.5 left-0.5 fill-transparent peer-checked:fill-white dark:fill-neutral-4">
+      <span className="block h-5 w-5 cursor-pointer rounded-sm border border-2 border-neutral-variant-50 animate-all ease-in-out duration-300 peer-checked:border-0 peer-checked:bg-primary-600 dark:border-neutral-varialnt-60 peer-checked:dark:bg-primary-200" />
+      <span className="absolute top-0.5 left-0.5 fill-transparent animate-all ease-in-out duration-300 peer-checked:fill-white dark:fill-neutral-4">
         <Icon icon="check" height={16} width={16} />
       </span>
     </label>
