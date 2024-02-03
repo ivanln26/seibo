6f445f904f57ffe07e059a04a59d36bff20ff455
Ivan Nuñez <ivanluis.cba@gmail.com>
2024-02-03 19:15:38 -0300

    (#96) Filter grades based on role in grade notif

--- src/app/[slug]/notification/grade/page.tsx
+++ src/app/[slug]/notification/grade/page.tsx
@@ -1,8 +1,10 @@
-import { db } from "@/db/db";
-import { grade } from "@/db/schema";
-import { getUserProfile } from "@/db/queries";
+import { and, eq } from "drizzle-orm";
 
 import Form from "./form";
+import { db } from "@/db/db";
+import { grade, instance, school, user } from "@/db/schema";
+import type { Grade } from "@/db/schema";
+import { getUserProfile, hasRoles } from "@/db/queries";
 
 export const revalidate = 0;
 
@@ -13,8 +15,44 @@ type Props = {
 };
 
 export default async function Page({ params }: Props) {
-  const user = await getUserProfile({ slug: params.slug });
-  const grades = await db.select().from(grade);
+  const userProfile = await getUserProfile({ slug: params.slug });
+  const isAdminOrPrincipal = await hasRoles(
+    userProfile,
+    "OR",
+    "admin",
+    "principal",
+  );
+  const isTeacher = await hasRoles(userProfile, "OR", "teacher");
 
-  return <Form user={user} slug={params.slug} grades={grades} />;
+  const selectData = {
+    id: grade.id,
+    schoolId: grade.schoolId,
+    name: grade.name,
+  };
+  let grades: Grade[] = [];
+  if (isAdminOrPrincipal) {
+    grades = await db
+      .select(selectData)
+      .from(grade)
+      .innerJoin(school, eq(grade.schoolId, school.id))
+      .where(eq(school.slug, params.slug));
+  } else if (isTeacher) {
+    grades = await db
+      .selectDistinct(selectData)
+      .from(grade)
+      .innerJoin(school, eq(grade.schoolId, school.id))
+      .innerJoin(instance, eq(grade.id, instance.gradeId))
+      .innerJoin(user, eq(instance.professorId, user.id))
+      .where(
+        and(
+          eq(school.slug, params.slug),
+          !isAdminOrPrincipal && isTeacher
+            ? eq(user.id, userProfile.id)
+            : undefined,
+        ),
+      );
+  }
+  // TODO: add isTutor
+
+  return <Form user={userProfile} slug={params.slug} grades={grades} />;
 }
