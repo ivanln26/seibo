70085b4eb92c5c472673338691458a65f1394258
Ivan Nuñez <ivanluis.cba@gmail.com>
2024-02-03 21:23:35 -0300

    (#101) Add drizzle schema relations

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -31,6 +31,7 @@ type SchoolSettings = {
 
 export const userRelations = relations(user, ({ many }) => ({
   profiles: many(schoolUser),
+  instances: many(instance),
 }));
 
 export const school = mysqlTable("school", {
@@ -44,7 +45,11 @@ export type School = typeof school.$inferSelect;
 export type NewSchool = typeof school.$inferInsert;
 
 export const schoolRelations = relations(school, ({ many }) => ({
+  classrooms: many(classroom),
+  courses: many(course),
+  grades: many(grade),
   profiles: many(schoolUser),
+  students: many(student),
 }));
 
 export type Role = "teacher" | "tutor" | "principal" | "admin";
@@ -94,6 +99,17 @@ export const student = mysqlTable("student", {
 export type Student = typeof student.$inferSelect;
 export type NewStudent = typeof student.$inferInsert;
 
+export const studentRelations = relations(student, ({ many, one }) => ({
+  attendances: many(attendance),
+  contacts: many(studentContact),
+  scores: many(score),
+  school: one(school, {
+    fields: [student.schoolId],
+    references: [school.id],
+  }),
+  studentGrades: many(studentGrade),
+}));
+
 export const studentContact = mysqlTable("student_contact", {
   id: int("id").autoincrement().primaryKey(),
   studentId: int("student_id").notNull(),
@@ -105,6 +121,13 @@ export const studentContact = mysqlTable("student_contact", {
 
 export type StudentContact = typeof studentContact.$inferSelect;
 export type NewStudentContact = typeof studentContact.$inferInsert;
+
+export const studentContactRelations = relations(studentContact, ({ one }) => ({
+  student: one(student, {
+    fields: [studentContact.studentId],
+    references: [student.id],
+  }),
+}));
 
 export const classroom = mysqlTable("classroom", {
   id: int("id").autoincrement().primaryKey(),
@@ -120,6 +143,14 @@ export const classroom = mysqlTable("classroom", {
 export type Classroom = typeof classroom.$inferSelect;
 export type NewClassroom = typeof classroom.$inferInsert;
 
+export const classroomRelations = relations(classroom, ({ many, one }) => ({
+  instances: many(instance),
+  school: one(school, {
+    fields: [classroom.schoolId],
+    references: [school.id],
+  }),
+}));
+
 export const grade = mysqlTable("grade", {
   id: int("id").autoincrement().primaryKey(),
   schoolId: int("school_id").notNull(),
@@ -128,6 +159,15 @@ export const grade = mysqlTable("grade", {
 
 export type Grade = typeof grade.$inferSelect;
 export type NewGrade = typeof grade.$inferInsert;
+
+export const gradeRelations = relations(grade, ({ many, one }) => ({
+  instances: many(instance),
+  school: one(school, {
+    fields: [grade.schoolId],
+    references: [school.id],
+  }),
+  studentGrades: many(studentGrade),
+}));
 
 export const studentGrade = mysqlTable("student_grade", {
   id: int("id").autoincrement().primaryKey(),
@@ -142,6 +182,17 @@ export const studentGrade = mysqlTable("student_grade", {
 
 export type StudentGrade = typeof studentGrade.$inferSelect;
 export type NewStudentGrade = typeof studentGrade.$inferInsert;
+
+export const studentGradeRelations = relations(studentGrade, ({ one }) => ({
+  grade: one(grade, {
+    fields: [studentGrade.gradeId],
+    references: [grade.id],
+  }),
+  student: one(student, {
+    fields: [studentGrade.studentId],
+    references: [student.id],
+  }),
+}));
 
 export const course = mysqlTable("course", {
   id: int("id").autoincrement().primaryKey(),
@@ -158,6 +209,14 @@ export const course = mysqlTable("course", {
 export type Course = typeof course.$inferSelect;
 export type NewCourse = typeof course.$inferInsert;
 
+export const courseRelations = relations(course, ({ many, one }) => ({
+  instances: many(instance),
+  school: one(school, {
+    fields: [course.schoolId],
+    references: [school.id],
+  }),
+}));
+
 export const instance = mysqlTable("instance", {
   id: int("id").autoincrement().primaryKey(),
   courseId: int("course_id").notNull(),
@@ -168,6 +227,27 @@ export const instance = mysqlTable("instance", {
 
 export type Instance = typeof instance.$inferSelect;
 export type NewInstance = typeof instance.$inferInsert;
+
+export const instanceRelations = relations(instance, ({ many, one }) => ({
+  classroom: one(classroom, {
+    fields: [instance.classroomId],
+    references: [classroom.id],
+  }),
+  course: one(course, {
+    fields: [instance.courseId],
+    references: [course.id],
+  }),
+  grade: one(grade, {
+    fields: [instance.gradeId],
+    references: [grade.id],
+  }),
+  schedules: many(schedule),
+  tests: many(test),
+  user: one(user, {
+    fields: [instance.professorId],
+    references: [user.id],
+  }),
+}));
 
 export const schedule = mysqlTable("schedule", {
   id: int("id").autoincrement().primaryKey(),
@@ -186,6 +266,14 @@ export const schedule = mysqlTable("schedule", {
 export type Schedule = typeof schedule.$inferSelect;
 export type NewSchedule = typeof schedule.$inferInsert;
 
+export const scheduleRelations = relations(schedule, ({ many, one }) => ({
+  instance: one(instance, {
+    fields: [schedule.instanceId],
+    references: [instance.id],
+  }),
+  lectures: many(lecture),
+}));
+
 export const lecture = mysqlTable("lecture", {
   id: int("id").autoincrement().primaryKey(),
   scheduleId: int("schedule_id").notNull(),
@@ -195,6 +283,14 @@ export const lecture = mysqlTable("lecture", {
 
 export type Lecture = typeof lecture.$inferSelect;
 export type NewLecture = typeof lecture.$inferInsert;
+
+export const lectureRelations = relations(lecture, ({ many, one }) => ({
+  attendances: many(attendance),
+  schedule: one(schedule, {
+    fields: [lecture.scheduleId],
+    references: [schedule.id],
+  }),
+}));
 
 export const attendance = mysqlTable("attendance", {
   id: int("id").autoincrement().primaryKey(),
@@ -206,6 +302,17 @@ export const attendance = mysqlTable("attendance", {
 export type Attendance = typeof attendance.$inferSelect;
 export type NewAttendance = typeof attendance.$inferInsert;
 
+export const attendanceRelations = relations(attendance, ({ one }) => ({
+  lecture: one(lecture, {
+    fields: [attendance.lectureId],
+    references: [lecture.id],
+  }),
+  student: one(student, {
+    fields: [attendance.studentId],
+    references: [student.id],
+  }),
+}));
+
 export const test = mysqlTable("test", {
   id: int("id").autoincrement().primaryKey(),
   instanceId: int("instance_id").notNull(),
@@ -216,6 +323,14 @@ export const test = mysqlTable("test", {
 
 export type Test = typeof test.$inferSelect;
 export type NewTest = typeof test.$inferInsert;
+
+export const testRelations = relations(test, ({ many, one }) => ({
+  instance: one(instance, {
+    fields: [test.instanceId],
+    references: [instance.id],
+  }),
+  scores: many(score),
+}));
 
 export const score = mysqlTable("score", {
   id: int("id").autoincrement().primaryKey(),
@@ -231,6 +346,17 @@ export const score = mysqlTable("score", {
 
 export type Score = typeof score.$inferSelect;
 export type NewScore = typeof score.$inferInsert;
+
+export const scoreRelations = relations(score, ({ one }) => ({
+  student: one(student, {
+    fields: [score.studentId],
+    references: [student.id],
+  }),
+  test: one(test, {
+    fields: [score.testId],
+    references: [test.id],
+  }),
+}));
 
 export const audit = mysqlTable("audit", {
   id: int("id").autoincrement().primaryKey(),
