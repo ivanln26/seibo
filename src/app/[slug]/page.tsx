5df2eae22dee858e9ab2ca6103c5e4b022a18f13
Ivan Nuñez <ivanluis.cba@gmail.com>
2024-02-09 16:57:18 -0300

    (#105) Use school colors in main page & button

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
+import type { TwColor } from "@/color";
 import Button from "@/components/button";
 import { db } from "@/db/db";
 import type { Role } from "@/db/schema";
@@ -13,23 +15,54 @@ type Props = {
   };
 };
 
-type Color = "primary" | "secondary" | "tertiary";
-
-const colors: Record<Color, string> = {
+const colors: Record<TwColor, string> = {
   primary:
     "bg-primary-100 text-primary-900 dark:bg-primary-700 dark:text-primary-100",
   secondary:
     "bg-secondary-100 text-secondary-900 dark:bg-secondary-700 dark:text-secondary-100",
-  tertiary:
-    "bg-tertiary-100 text-tertiary-900 dark:bg-tertiary-700 dark:text-tertiary-100",
+  red: "bg-red-100 text-red-900 dark:bg-red-700 dark:text-red-100",
+  orange:
+    "bg-orange-100 text-orange-900 dark:bg-orange-700 dark:text-orange-100",
+  yellow:
+    "bg-yellow-100 text-yellow-900 dark:bg-yellow-700 dark:text-yellow-100",
+  lime: "bg-lime-100 text-lime-900 dark:bg-lime-700 dark:text-lime-100",
+  green: "bg-green-100 text-green-900 dark:bg-green-700 dark:text-green-100",
+  emerald:
+    "bg-emerald-100 text-emerald-900 dark:bg-emerald-700 dark:text-emerald-100",
+  cyan: "bg-cyan-100 text-cyan-900 dark:bg-cyan-700 dark:text-cyan-100",
+  blue: "bg-blue-100 text-blue-900 dark:bg-blue-700 dark:text-blue-100",
+  violet:
+    "bg-violet-100 text-violet-900 dark:bg-violet-700 dark:text-violet-100",
+  purple:
+    "bg-purple-100 text-purple-900 dark:bg-purple-700 dark:text-purple-100",
+  fuchsia:
+    "bg-fuchsia-100 text-fuchsia-900 dark:bg-fuchsia-700 dark:text-fuchsia-100",
+  pink: "bg-pink-100 text-pink-900 dark:bg-pink-700 dark:text-pink-100",
 };
 
 type CardProps = {
   title: string;
   description: string;
   path: string;
-  color?: Color;
+  color?: "primary" | "secondary";
   roles?: Role[];
+};
+
+const subtitleColors: Record<TwColor, string> = {
+  primary: "text-primary-600 dark:text-primary-200",
+  secondary: "text-secondary-600 dark:text-secondary-200",
+  red: "text-red-600 dark:text-red-200",
+  orange: "text-orange-600 dark:text-orange-200",
+  yellow: "text-yellow-600 dark:text-yellow-200",
+  lime: "text-lime-600 dark:text-lime-200",
+  green: "text-green-600 dark:text-green-200",
+  emerald: "text-emerald-600 dark:text-emerald-200",
+  cyan: "text-cyan-600 dark:text-cyan-200",
+  blue: "text-blue-600 dark:text-blue-200",
+  violet: "text-violet-600 dark:text-violet-200",
+  purple: "text-purple-600 dark:text-purple-200",
+  fuchsia: "text-fuchsia-600 dark:text-fuchsia-200",
+  pink: "text-pink-600 dark:text-pink-200",
 };
 
 export default async function Home({ params }: Props) {
@@ -38,6 +71,13 @@ export default async function Home({ params }: Props) {
   const school = await db.query.school.findFirst({
     where: (sc, { eq }) => eq(sc.slug, params.slug),
   });
+
+  if (school === undefined) {
+    redirect("/");
+  }
+
+  const primaryColor = school.settings?.primary || "primary";
+  const secondaryColor = school.settings?.secondary || "secondary";
 
   const cards: CardProps[] = [
     {
@@ -85,7 +125,7 @@ export default async function Home({ params }: Props) {
       <h1 className="text-4xl font-bold">
         Bienvenido a SEIBO &#128394;
       </h1>
-      <p className="text-primary-600 dark:text-primary-200">
+      <p className={subtitleColors[primaryColor]}>
         Gestiona fácilmente la información de la institución{" "}
         <b>{school?.name}</b>
       </p>
@@ -96,14 +136,16 @@ export default async function Home({ params }: Props) {
           (
             <div
               className={`flex flex-col justify-between gap-y-2 px-4 py-6 rounded ${
-                card.color ? colors[card.color] : colors["primary"]
+                card.color === "primary"
+                  ? colors[primaryColor]
+                  : colors[secondaryColor]
               }`}
               key={i}
             >
               <h2 className="text-xl font-bold">{card.title}</h2>
               <p>{card.description}</p>
               <Link href={card.path}>
-                <Button color={card.color}>Ver detalle</Button>
+                <Button color={primaryColor}>Ver detalle</Button>
               </Link>
             </div>
           )
