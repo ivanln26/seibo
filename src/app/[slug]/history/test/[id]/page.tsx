6f85f8db74e536a15a15746ef92ac55ede664984
Ivan Nuñez <ivanluis.cba@gmail.com>
2024-02-20 23:43:08 -0300

    (#108) Create test history detail page

--- src/app/[slug]/history/test/[id]/page.tsx
+++ src/app/[slug]/history/test/[id]/page.tsx
@@ -4,17 +4,15 @@ import { Fragment } from "react";
 
 import { db } from "@/db/db";
 import {
-  attendance,
   classroom,
   course,
   grade,
   instance,
-  lecture,
-  schedule,
   school,
+  score,
   student,
+  test,
   user,
-  weekdayTranslate,
 } from "@/db/schema";
 
 export const revalidate = 0;
@@ -28,88 +26,81 @@ type Props = {
 
 export default async function Page({ params }: Props) {
   if (isNaN(Number(params.id))) {
-    redirect(`/${params.slug}/history/lecture`);
+    redirect(`/${params.slug}/history/test`);
   }
 
   const query = await db
     .select({
-      id: lecture.id,
-      date: sql<string>`${lecture.date}`,
-      notes: lecture.notes,
-      weekday: schedule.weekday,
+      id: test.id,
+      title: test.title,
+      topics: test.topics,
+      date: sql<string>`${test.date}`,
       classroom: classroom.name,
       course: course.name,
       grade: grade.name,
       professor: user.name,
     })
-    .from(lecture)
-    .innerJoin(schedule, eq(lecture.scheduleId, schedule.id))
-    .innerJoin(instance, eq(schedule.instanceId, instance.id))
+    .from(test)
+    .innerJoin(instance, eq(test.instanceId, instance.id))
     .innerJoin(classroom, eq(instance.classroomId, classroom.id))
     .innerJoin(course, eq(instance.courseId, course.id))
-    .innerJoin(user, eq(instance.professorId, user.id))
     .innerJoin(grade, eq(instance.gradeId, grade.id))
+    .innerJoin(user, eq(instance.professorId, user.id))
     .innerJoin(school, eq(grade.schoolId, school.id))
     .where(and(
       eq(school.slug, params.slug),
-      eq(lecture.id, Number(params.id)),
+      eq(test.id, Number(params.id)),
     ));
 
   if (query.length !== 1) {
-    redirect(`/${params.slug}/history/lecture`);
+    redirect(`/${params.slug}/history/test}`);
   }
 
-  const attendances = await db
+  const scores = await db
     .select({
-      id: attendance.id,
-      isPresent: attendance.isPresent,
+      id: score.id,
+      score: score.score,
       student: sql<
         string
       >`CONCAT_WS(", ", ${student.lastName}, ${student.firstName})`,
     })
-    .from(attendance)
-    .innerJoin(lecture, eq(attendance.lectureId, lecture.id))
-    .innerJoin(student, eq(attendance.studentId, student.id))
-    .where(eq(lecture.id, Number(params.id)))
-    .orderBy(student.lastName, student.lastName);
+    .from(score)
+    .innerJoin(test, eq(score.testId, test.id))
+    .innerJoin(student, eq(score.studentId, student.id))
+    .where(eq(test.id, Number(params.id)))
+    .orderBy(student.lastName, student.firstName);
 
   return (
     <>
-      <h1 className="text-4xl">Asistencia</h1>
+      <h1 className="text-4xl">Examen</h1>
+      <p>Título: {query[0].title}</p>
       <p>Materia: {query[0].course}</p>
       <p>Profesor: {query[0].professor}</p>
       <p>Aula: {query[0].classroom}</p>
       <p>Curso: {query[0].grade}</p>
-      <p>
-        Día: {query[0].date.replaceAll("-", "/")}{" "}
-        ({weekdayTranslate[query[0].weekday]})
-      </p>
-      <h2 className="py-1 text-2xl">Asistencias</h2>
-      {attendances.length === 0
+      <p>Día: {query[0].date}</p>
+      <h2 className="py-1 text-2xl">Notas</h2>
+      {scores.length === 0
         ? (
           <p className="text-error-600 dark:text-error-200">
-            No se cargaron asistencias esta clase.
+            No se cargaron notas.
           </p>
         )
         : (
           <ul>
-            {attendances.map(({ id, student, isPresent }, i) => (
+            {scores.map(({ id, student, score }, i) => (
               <Fragment key={id}>
                 <li className="flex justify-between py-1 text-2xl font-mono">
                   {student}
-                  <span>
-                    {isPresent ? "Presente" : "Ausente"}
-                  </span>
+                  <span>{score}</span>
                 </li>
-                {i + 1 !== attendances.length && (
+                {i + 1 !== scores.length && (
                   <hr className="border border-neutral-variant-50 dark:border-neutral-variant-60" />
                 )}
               </Fragment>
             ))}
           </ul>
         )}
-      <h2 className="py-1 text-2xl">Observaciones</h2>
-      <p className="font-mono">{query[0].notes}</p>
     </>
   );
 }
